49c08e2a8a0f4545cbdfc3ed2236d9d83aa77393
chip <49256163+chipanyanwu@users.noreply.github.com>
2024-11-20 19:54:20 -0600

    restructure home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,12 @@ import StatusBar from "./components/StatusBar"
 import Slider from "./components/ui/Slider"
 import Tamagotchi from "./components/Tamagotchi"
 import ActionsBar from "./components/ActionsBar"
+
 export default function Home() {
-  const [percentage, setPercentage] = useState(25)
+  const [hungerPercentage, setHungerPercentage] = useState(100)
+  const [happinessPercentage, setHappinessPercentage] = useState(100)
+  const [cleanlinessPercentage, setCleanlinessPercentage] = useState(100)
+
   const [name, setName] = useState("Tamagotchi")
   const [hunger, setHunger] = useState(100)
   const [happiness, setHappiness] = useState(100)
@@ -15,21 +19,29 @@ export default function Home() {
   const [index_x, setIndex_x] = useState(0)
   const [index_y, setIndex_y] = useState(0)
   const [showNav, setShowNav] = useState(false)
+
   return (
     <div className="w-screen h-screen flex justify-center items-center flex-col gap-4">
-      <button className='appButton' onClick={()=> setShowNav(!showNav)} onTouchStart={()=>setShowNav(!showNav)}>Actions</button>
-      <StatusBar percentage={percentage} statName="Health" />
-      <div className="space-y-2">
-        <label
-          htmlFor="percentage-slider"
-          className="text-sm font-medium text-gray-700"
-        >
-          Adjust Percentage
-        </label>
-        <Slider value={percentage} onChange={setPercentage} />
-      </div>
-      <Tamagotchi name={name} hunger={hunger} happiness={happiness} cleanliness={cleanliness} growth={growth} index_x={index_x} index_y={index_y} />
-      <ActionsBar showing={showNav}/>
+      <StatusBar percentage={hungerPercentage} statName="Hunger" />
+      <StatusBar percentage={happinessPercentage} statName="Happiness" />
+      <StatusBar percentage={cleanlinessPercentage} statName="Cleanliness" />
+      <Tamagotchi
+        name={name}
+        hunger={hunger}
+        happiness={happiness}
+        cleanliness={cleanliness}
+        growth={growth}
+        index_x={index_x}
+        index_y={index_y}
+      />
+      <button
+        className="appButton"
+        onClick={() => setShowNav(!showNav)}
+        onTouchStart={() => setShowNav(!showNav)}
+      >
+        Actions
+      </button>
+      <ActionsBar showing={showNav} />
     </div>
   )
 }
