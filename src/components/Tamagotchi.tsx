6068ab6eb36d481bf6a8238e2e81b9b5583bf407
yeeet0421 <nctucs.cs08@nycu.edu.tw>
2024-11-20 18:49:19 -0600

    added props for tamagotchi

--- src/components/Tamagotchi.tsx
+++ src/components/Tamagotchi.tsx
@@ -1,5 +1,33 @@
-const Tamogatchi = () => {
-  return <div>Tamogatchi</div>
+// import { useState } from "react";
+
+interface Props {
+  name: string;
+  hunger: number;
+  happiness: number;
+  cleanliness: number;
+  growth: string;
+  index_x: number;
+  index_y: number;
+}
+const Tamagotchi = (props : Props) => {
+  console.log(props);
+
+  const imageStyle:React.CSSProperties = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: `translate(${props.index_x-112.5}px, ${props.index_y-112.5}px)`,
+  };
+
+  return (
+  <>
+    <img
+     src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgkC6eik7wJKas6zKgVmEehFpFcRzE28n0qg&s"} 
+     alt="tamagotchi" 
+     style={imageStyle}
+     />
+  </>
+  )
 }
 
-export default Tamogatchi
+export default Tamagotchi
